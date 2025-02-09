140afa155ae9331cf89dee1c34c138d41ca5ffb1
Lorenzo Karel <lorenzok@quanza.net>
2025-02-09 15:05:55 +0100

    Working on settings for game

---
+++ src/settings.rs
@@ -0,0 +1,9 @@
+// Settings for Rusty Snake. 
+// TOOD: Making clarity in structure 
+
+pub const CELL_SIZE: f32 = 20.0;
+pub const GRID_WIDTH: i32 = 50;
+pub const GRID_HEIGHT: i32 = 40;
+pub const WINDOW_WIDTH: f32 = CELL_SIZE * GRID_WIDTH as f32;
+pub const WINDOW_HEIGHT: f32 = CELL_SIZE * GRID_HEIGHT as f32;
+pub const MOVE_INTERVAL: f32 = 0.1;
